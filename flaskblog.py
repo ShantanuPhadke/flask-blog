cb3fda660d5f4b9909625e90e4a319d012761529
Shantanu Phadke <shantanu.phadke@berkeley.edu>
2019-08-17 08:16:07 -0700

    Video 2: Added in Home and About templates + layout.html (#1)

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,32 @@
-from flask import Flask 
+from flask import Flask , render_template
 app = Flask(__name__)
+
+# Dummy post data, valuable because it tells us the proper
+# schema for individual Post Objects
+posts = [
+	{
+		'author': 'Corey Schafer',
+		'title': 'Blog Post 1',
+		'content': 'First Post Content!',
+		'date_posted': 'August 17, 2019'
+	},
+
+	{
+		'author': 'Jane Doe',
+		'title': 'Blog Post 2',
+		'content': 'Second Post Content!',
+		'date_posted': 'August 18, 2019'
+	}
+]
 
 @app.route("/")
 @app.route("/home")
 def hello():
-	return "<h1>Hello World!</h1>"
+	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-	return "<h1>About Page</h1>"
+	return render_template('about.html', title="About")
 
 if __name__ == '__main__':
 	app.run(debug=True)
