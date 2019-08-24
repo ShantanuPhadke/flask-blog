4db059061af1f533b69a67e3d8e653b188e4d493
Shantanu Phadke <shantanu.phadke@berkeley.edu>
2019-08-23 18:40:57 -0700

    Scratch/video 3 forms user input (#2)
    * Video 2: Added in Home and About templates + layout.html
    
    * Login/Registration Forms added in

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '8479fd7bef4ebf6c264fd36063af37b8'
 # Dummy post data, valuable because it tells us the proper
 # schema for individual Post Objects
 posts = [
@@ -21,12 +23,33 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
 	return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
 	return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	registration_form = RegistrationForm()
+	if registration_form.validate_on_submit():
+		flash(f'Account created for {registration_form.username.data}!', 'success')
+		return redirect(url_for('home'))
+	return render_template('register.html', title='Register', form=registration_form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	login_form = LoginForm()
+	# SAMPLE VALIDATION BELOW
+	if login_form.validate_on_submit():
+		if login_form.email.data == 'admin@blog.com' and login_form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password', 'danger')
+	# SAMPLE VALIDATION ABOVE
+	return render_template('login.html', title='Login', form=login_form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
