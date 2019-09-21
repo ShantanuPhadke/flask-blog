129db17430061df1cb4b53798a13d89d9770d253
Shantanu Phadke <shantanu.phadke@berkeley.edu>
2019-09-21 10:59:00 -0700

    Added in User Authentication (bcrypt, actual registration/login, route restriction) (#4)

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
-
+# Importing in the bcrypt module
+from flask_bcrypt import Bcrypt 
+from flask_login import LoginManager
 
 # Application and Database Configuration below here
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8479fd7bef4ebf6c264fd36063af37b8'
-app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+# Making a bcrypt object for the app
+bcrypt = Bcrypt(app)
+# Making a LoginManager object for the app
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' # default view to show when login_required is not met
+login_manager.login_message_category = 'info' # Formatting the message that is shown
+
 # Application and Database Configuration above here
 
 from flaskblog import routes
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,8 +1,17 @@
-from flaskblog import db
+from flaskblog import db, login_manager
 from datetime import datetime 
+from flask_login import UserMixin 
 
+# Helper function that takes in an user id as input
+# and returns the corresponding User object. It is 
+# needed for the Flask's LoginManager needs to work
+# properly.
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
+ 
 # Defining the Schema for the User Database Model
-class User(db.Model):
+class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
